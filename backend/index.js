dfcf58af847d8a6b039775d8740554d497fa7f5f
papa-react <makiya112545@gmail.com>
2022-03-10 18:35:05 +0300

    user api created

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/connectDB");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 const port = "5000";
 
 dotenv.config();
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
 
 app.listen(port, () => {
 	console.log(`backend server ${"http://localhost:" + port} `);
