d9bdf58b6fa814827db8317bcf22af4ec1ef5a4c
papa-react <makiya112545@gmail.com>
2022-03-10 12:18:32 +0300

    controller creatd

---
+++ backend/controller/postController.js
@@ -0,0 +1,15 @@
+// GET
+const getUser = (req, res) => {};
+// SET
+const setUser = (req, res) => {};
+// DELETE
+const deleteUser = (req, res) => {};
+// UPDATE
+const updateUser = (req, res) => {};
+
+module.exports = {
+	getUser,
+	setUser,
+	deleteUser,
+	updateUser,
+};
