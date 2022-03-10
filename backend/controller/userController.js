44c63f6de273cc4a3c480763e2ca617160ea3cfd
papa-react <makiya112545@gmail.com>
2022-03-10 18:35:46 +0300

    update user completed

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,11 +1,49 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../model/User");
+const bcrypt = require("bcrypt");
 // GET
-const getUser = (req, res) => {};
+const getUser = asyncHandler(async (req, res) => {
+	try {
+	} catch (error) {}
+});
 // SET
-const setUser = (req, res) => {};
+const setUser = asyncHandler(async (req, res) => {});
 // DELETE
-const deleteUser = (req, res) => {};
+const deleteUser = asyncHandler(async (req, res) => {});
 // UPDATE
-const updateUser = (req, res) => {};
+const updateUser = asyncHandler(async (req, res) => {
+	if (req.body.userID === req.params.id) {
+		var { userID, username, email, password, profilePic } = req.body;
+		if (username || email || profilePic || password) {
+			if (password) {
+				// hash password
+				const salt = await bcrypt.genSalt(10);
+				password = await bcrypt.hash(password, salt);
+			}
+			try {
+				// new data
+				const userData = {
+					username,
+					email,
+					profilePic,
+					password,
+				};
+				const user = await User.findByIdAndUpdate(userID, userData, {
+					new: true,
+				});
+				res.status(200).json(user);
+			} catch (error) {}	
+		} else {
+			res.status("401").json({
+				message: "write what you want to update!",
+			});
+		}
+	} else {
+		res.status("401").json({
+			message: "you can update only our account !",
+		});
+	}
+});
 
 module.exports = {
 	getUser,
