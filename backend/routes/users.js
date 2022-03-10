e8f4e83d0897e856369e09444abb9228b51b8e18
papa-react <makiya112545@gmail.com>
2022-03-10 20:10:21 +0300

    get user  by  id

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 // controller
-const { deleteUser, updateUser } = require("../controller/userController");
+const {
+	deleteUser,
+	updateUser,
+	getUser,
+} = require("../controller/userController");
 
+router.get("/:id", getUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
