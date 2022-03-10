0e04fb028d68c8ecfeafbecb2132ae54c6bde006
papa-react <makiya112545@gmail.com>
2022-03-10 22:40:49 +0300

    create post completed

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -1,15 +1,51 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../model/User");
+const Post = require("../model/Post");
+//  create post
+const createPost = asyncHandler(async (req, res) => {
+	try {
+		const { title, desc, username, photo, categories } = req.body;
+		if (!username || !title || !desc) {
+			res.status(400).json({ message: "fill required fields" });
+		}
+		const post = await Post.create({
+			title,
+			desc,
+			username,
+			photo,
+			categories,
+		});
+		res.status(200).json(post);
+	} catch (error) {
+		res.status(400).json({
+			message: "title already taken!",
+		});
+	}
+});
 // GET
-const getUser = (req, res) => {};
-// SET
-const setUser = (req, res) => {};
-// DELETE
-const deleteUser = (req, res) => {};
-// UPDATE
-const updateUser = (req, res) => {};
+const getPost = asyncHandler(async (req, res) => {
+	// if (req.body.userID === req.params.id) {
+	// 	try {
+	// 		var user = await User.findById(req.params.id);
+	// 		if (!user) {
+	// 			res.status(401).json({ message: "User NOT Found" });
+	// 		}
+	// 		const { password, ...others } = user._doc;
+	// 		res.status(200).json(others);
+	// 	} catch (error) {
+	// 		res.status(401).json(error);
+	// 	}
+	// } else {
+	// 	res.status(401).json({ message: "you can Get only  your account" });
+	// }
+});
+
+const deletePost = asyncHandler(async (req, res) => {});
+const updatePost = asyncHandler(async (req, res) => {});
 
 module.exports = {
-	getUser,
-	setUser,
-	deleteUser,
-	updateUser,
+	getPost,
+	createPost,
+	deletePost,
+	updatePost,
 };
