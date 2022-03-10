181c113e575da2a48812b0856ffa2899ce6558b3
papa-react <makiya112545@gmail.com>
2022-03-10 17:29:09 +0300

    clean codes

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/connectDB");
-// start env file
-dotenv.config();
-//  connect database
-connectDB();
-// port
+const authRoute = require("./routes/auth");
 const port = "5000";
-// start express app
-const app = express();
-// middleware
-app.use(express.json());
 
-app.use("/api/auth", require("./routes/auth"));
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use("/api/auth", authRoute);
 
 app.listen(port, () => {
 	console.log(`backend server ${"http://localhost:" + port} `);
