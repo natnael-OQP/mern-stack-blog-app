a8eac02e5edaef2c79b9e4ff0a1658553f35ee3e
papa-react <makiya112545@gmail.com>
2022-03-10 17:30:20 +0300

    add login router

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
-const { register } = require("../controller/registerController");
+const { register, login } = require("../controller/authController");
 const router = require("express").Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
