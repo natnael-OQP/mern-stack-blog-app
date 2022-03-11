1bf28170ff6bcb6575ce26060b370154b63cc3c0
papa-react <makiya112545@gmail.com>
2022-03-11 18:09:04 +0300

    get data from api

--- frontend/src/components/singlePost/SinglePost.jsx
+++ frontend/src/components/singlePost/SinglePost.jsx
@@ -2,17 +2,23 @@ import { Link } from "react-router-dom";
 import "./singlePost.css";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 
-export default function Post() {
+export default function Post({
+	photo,
+	username,
+	categories,
+	createdAt,
+	desc,
+	title,
+	_id,
+}) {
 	return (
 		<div className="singlePost">
 			<div className="singlePostWrapper">
-				<img
-					className="singlePostImg"
-					src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-					alt=""
-				/>
+				{photo && (
+					<img className="singlePostImg" src={photo} alt={title} />
+				)}
 				<h1 className="singlePostTitle">
-					Lorem ipsum dolor
+					{title}
 					<div className="singlePostEdit">
 						<span className="singlePostIcon">
 							<FaEdit />
@@ -26,48 +32,17 @@ export default function Post() {
 					<span>
 						Author:
 						<b className="singlePostAuthor">
-							<Link className="link" to="/posts?username=Safak">
-								Safak
+							<Link
+								className="link"
+								to={`/posts?user=${username}`}
+							>
+								{username}
 							</Link>
 						</b>
 					</span>
-					<span>1 day ago</span>
+					<span>{new Date(createdAt).toDateString()}</span>
 				</div>
-				<p className="singlePostDesc">
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-					Iste error quibusdam ipsa quis quidem doloribus eos, dolore
-					ea iusto impedit! Voluptatum necessitatibus eum beatae,
-					adipisci voluptas a odit modi eos! Lorem, ipsum dolor sit
-					amet consectetur adipisicing elit. Iste error quibusdam ipsa
-					quis quidem doloribus eos, dolore ea iusto impedit!
-					Voluptatum necessitatibus eum beatae, adipisci voluptas a
-					odit modi eos! Lorem, ipsum dolor sit amet consectetur
-					adipisicing elit. Iste error quibusdam ipsa quis quidem
-					doloribus eos, dolore ea iusto impedit! Voluptatum
-					necessitatibus eum beatae, adipisci voluptas a odit modi
-					eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-					elit. Iste error quibusdam ipsa quis quidem doloribus eos,
-					dolore ea iusto impedit! Voluptatum necessitatibus eum
-					beatae, adipisci voluptas a odit modi eos! Lorem, ipsum
-					dolor sit amet consectetur adipisicing elit. Iste error
-					quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
-					impedit! Voluptatum necessitatibus eum beatae, adipisci
-					voluptas a odit modi eos!
-					<br />
-					<br />
-					Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-					Iste error quibusdam ipsa quis quidem doloribus eos, dolore
-					ea iusto impedit! Voluptatum necessitatibus eum beatae,
-					adipisci voluptas a odit modi eos! Lorem, ipsum dolor sit
-					amet consectetur adipisicing elit. Iste error quibusdam ipsa
-					quis quidem doloribus eos, dolore ea iusto impedit!
-					Voluptatum necessitatibus eum beatae, adipisci voluptas a
-					odit modi eos! Lorem, ipsum dolor sit amet consectetur
-					adipisicing elit. Iste error quibusdam ipsa quis quidem
-					doloribus eos, dolore ea iusto impedit! Voluptatum
-					necessitatibus eum beatae, adipisci voluptas a odit modi
-					eos! Lorem, ipsum dolor sit amet consectetur.
-				</p>
+				<p className="singlePostDesc">{desc}</p>
 			</div>
 		</div>
 	);
