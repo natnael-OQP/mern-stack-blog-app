3ed85c5b76defa31f9800aa53785bd969aed14d2
papa-react <makiya112545@gmail.com>
2022-03-11 16:22:04 +0300

    fetch posts and pass to posts

--- frontend/src/pages/homepage/Homepage.jsx
+++ frontend/src/pages/homepage/Homepage.jsx
@@ -1,17 +1,31 @@
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router";
 import Banner from "../../components/banner/Banner";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./homepage.css";
+import axios from "axios";
+import Spinner from "../../components/spinner/Spinner";
 
 export default function Homepage() {
+	const [posts, setPosts] = useState();
 	const location = useLocation();
-	console.log(location);
+
+	useEffect(() => {
+		const fetcher = async () => {
+			const { data } = await axios.get("posts");
+			setPosts(data);
+		};
+		fetcher();
+	}, []);
+	console.log(posts);
+	if (!posts) return <Spinner />;
+
 	return (
 		<>
 			<Banner />
 			<div className="home">
-				<Posts />
+				<Posts posts={posts} />
 				<Sidebar />
 			</div>
 		</>
