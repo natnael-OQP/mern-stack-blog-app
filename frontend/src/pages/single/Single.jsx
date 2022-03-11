4dcf166cdc8dcb852c31ccbc52f62379c8311f98
papa-react <makiya112545@gmail.com>
2022-03-11 18:12:11 +0300

    fetch post by id

--- frontend/src/pages/single/Single.jsx
+++ frontend/src/pages/single/Single.jsx
@@ -1,11 +1,26 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Sidebar from "../../components/sidebar/Sidebar";
 import SinglePost from "../../components/singlePost/SinglePost";
+import Spinner from "../../components/spinner/Spinner";
 import "./single.css";
 
 export default function Single() {
+	const location = useLocation();
+	const id = location.pathname.split("/")[2];
+	const [post, setPost] = useState();
+	useEffect(() => {
+		const fetcher = async () => {
+			const { data } = await axios.get(`/posts/${id}`);
+			setPost(data);
+		};
+		fetcher();
+	}, []);
+	if (!post) return <Spinner />;
 	return (
 		<div className="single">
-			<SinglePost />
+			<SinglePost {...post} />
 			<Sidebar />
 		</div>
 	);
