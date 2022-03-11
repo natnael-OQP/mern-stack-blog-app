d468573335da3760152bab2a073780ad67aac870
papa-react <makiya112545@gmail.com>
2022-03-11 10:02:45 +0300

    category controller completed

--- backend/controller/categoryController.js
+++ backend/controller/categoryController.js
@@ -1,15 +1,31 @@
-// GET
-const getUser = (req, res) => {};
-// SET
-const setUser = (req, res) => {};
-// DELETE
-const deleteUser = (req, res) => {};
-// UPDATE
-const updateUser = (req, res) => {};
+const asyncHandler = require("express-async-handler");
+const Category = require("../model/Category");
+
+// get category
+const getCategory = asyncHandler(async (req, res) => {
+	try {
+		const categories = await Category.find();
+		res.status(200).json(categories);
+	} catch (error) {
+		res.status(404).json({ message: "Category Not Found!" });
+	}
+});
+// Create category
+const createCategory = asyncHandler(async (req, res) => {
+	try {
+		const { name } = req.body;
+		//  check if request body is empty
+		if (!name) {
+			res.status(400).json({ message: "fill required fields" });
+		}
+		const newCategory = await Category.create({ name });
+		res.status(200).json(newCategory);
+	} catch (error) {
+		res.status(400).json({ message: "category already exists!" });
+	}
+});
 
 module.exports = {
-	getUser,
-	setUser,
-	deleteUser,
-	updateUser,
+	getCategory,
+	createCategory,
 };
