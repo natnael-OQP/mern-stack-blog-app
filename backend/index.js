bac4cb2ffe232cf66a011344f5c23891f578d1de
papa-react <makiya112545@gmail.com>
2022-03-11 10:02:06 +0300

    create categories api

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const connectDB = require("./config/connectDB");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+
 const port = "5000";
 
 dotenv.config();
@@ -15,6 +17,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen(port, () => {
 	console.log(`backend server ${"http://localhost:" + port} `);
