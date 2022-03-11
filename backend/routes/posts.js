832305422ef5c359344b9818765ff506121033f6
papa-react <makiya112545@gmail.com>
2022-03-11 07:56:06 +0300

    crud posts routes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
-const { createPost } = require("../controller/postController");
+const {
+	createPost,
+	deletePost,
+	updatePost,
+	getPost,
+} = require("../controller/postController");
 
 router.post("/", createPost);
+router.delete("/:id", deletePost);
+router.put("/:id", updatePost);
+router.get("/:id", getPost);
 
 module.exports = router;
