bb6fa8fb845e4ded176186b6db46aa6b7e9b9acf
papa-react <makiya112545@gmail.com>
2022-03-12 21:55:54 +0300

    create post

--- frontend/src/pages/write/Write.jsx
+++ frontend/src/pages/write/Write.jsx
@@ -1,14 +1,51 @@
-import "./write.css";
+import { useContext, useState } from "react";
 import { FaPlus } from "react-icons/fa";
+import "./write.css";
+import axios from "axios";
+import { Context } from "../../context/Context";
+
 export default function Write() {
+	const [file, setFile] = useState();
+	const [input, setInput] = useState();
+	const [image, setImage] = useState();
+	const [loading, setLoding] = useState();
+	const { user } = useContext(Context);
+
+	const handelChange = (e) => {
+		setInput((prev) => ({
+			...prev,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	const handelSubmit = async (e) => {
+		e.preventDefault();
+		const data = new FormData();
+		data.append("file", file);
+		data.append("upload_preset", "uploads");
+		try {
+			const upload = await axios.post(
+				`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_ID}/image/upload`,
+				data
+			);
+			const { url } = upload.data;
+			setImage(url);
+			const post = {
+				...input,
+				photo: url,
+				username: user.username,
+			};
+			const { data: posts } = await axios.post("/posts", post);
+			window.location.replace("/post/" + posts._id);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<div className="write">
-			<img
-				className="writeImg"
-				src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-				alt=""
-			/>
-			<form className="writeForm">
+			{image && <img className="writeImg" src={image} alt="" />}
+			<form onSubmit={handelSubmit} className="writeForm">
 				<div className="writeFormGroup">
 					<label htmlFor="fileInput">
 						<FaPlus
@@ -20,12 +57,15 @@ export default function Write() {
 					<input
 						id="fileInput"
 						type="file"
+						onChange={(e) => setFile(e.target.files[0])}
 						style={{ display: "none" }}
 					/>
 					<input
 						className="writeInput"
 						placeholder="Title"
 						type="text"
+						onChange={handelChange}
+						name="title"
 						autoFocus={true}
 					/>
 				</div>
@@ -34,6 +74,8 @@ export default function Write() {
 						className="writeInput writeText"
 						placeholder="Tell your story..."
 						type="text"
+						onChange={handelChange}
+						name="desc"
 						autoFocus={true}
 					/>
 				</div>
