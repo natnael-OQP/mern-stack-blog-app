49fddec01f8720d466266701d9f0c3aa21b1f631
papa-react <makiya112545@gmail.com>
2022-03-12 21:48:10 +0300

    get user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import Header from "./components/header/Header";
@@ -9,9 +9,11 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Context } from "./context/Context";
 
 function App() {
-	const user = false;
+	const { user } = useContext(Context);
+
 	return (
 		<Router>
 			<Header />
