016480d3ff47729dba6dafcdd44dea2785cb20b0
papa-react <makiya112545@gmail.com>
2022-03-13 17:14:41 +0300

    update postes

--- frontend/src/components/singlePost/SinglePost.jsx
+++ frontend/src/components/singlePost/SinglePost.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import "./singlePost.css";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
@@ -14,6 +14,10 @@ export default function Post({
 	title,
 	_id,
 }) {
+	const [newTitle, setTitle] = useState(title);
+	const [newDesc, setDesc] = useState(desc);
+	const [updateMode, setUpdateMode] = useState(false);
+
 	const { user } = useContext(Context);
 	const handelDelete = async () => {
 		try {
@@ -23,43 +27,84 @@ export default function Post({
 			console.log(error);
 		}
 	};
+
+	const handleUpdate = async () => {
+		try {
+			if (newDesc || newTitle) {
+				setTitle(newTitle);
+				await axios.put("http://localhost:5000/api/posts/" + _id, {
+					username: user.username,
+					title: newTitle,
+					desc: newDesc,
+				});
+				setUpdateMode(false);
+				window.location.reload();
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<div className="singlePost">
 			<div className="singlePostWrapper">
 				{photo && (
 					<img className="singlePostImg" src={photo} alt={title} />
 				)}
-				<h1 className="singlePostTitle">
-					{title}
-					{username === user.username && (
-						<div className="singlePostEdit">
-							<span className="singlePostIcon">
-								<FaEdit />
-							</span>
-							<span
-								onClick={handelDelete}
-								className="singlePostIcon"
-							>
-								<FaTrashAlt />
-							</span>
-						</div>
-					)}
-				</h1>
+				{updateMode ? (
+					<input
+						type="text"
+						value={newTitle}
+						className="singlePostTitleInput"
+						autoFocus
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				) : (
+					<h1 className="singlePostTitle">
+						{title}
+						{username === user.username && (
+							<div className="singlePostEdit">
+								<span className="singlePostIcon">
+									<FaEdit
+										onClick={() => setUpdateMode(true)}
+									/>
+								</span>
+								<span
+									onClick={handelDelete}
+									className="singlePostIcon"
+								>
+									<FaTrashAlt />
+								</span>
+							</div>
+						)}
+					</h1>
+				)}
 				<div className="singlePostInfo">
-					<span>
+					<span className="singlePostAuthor">
 						Author:
-						<b className="singlePostAuthor">
-							<Link
-								className="link"
-								to={`/posts?user=${username}`}
-							>
-								{username}
-							</Link>
-						</b>
+						<Link to={`/?user=${username}`} className="link">
+							<b> {username}</b>
+						</Link>
 					</span>
-					<span>{new Date(createdAt).toDateString()}</span>
+					<span className="singlePostDate">
+						{new Date(createdAt).toDateString()}
+					</span>
 				</div>
-				<p className="singlePostDesc">{desc}</p>
+				{updateMode ? (
+					<textarea
+						className="singlePostDescInput"
+						value={newDesc}
+						rows={10}
+						onChange={(e) => setDesc(e.target.value)}
+					/>
+				) : (
+					<p className="singlePostDesc">{desc}</p>
+				)}
+				{updateMode && (
+					<button className="singlePostButton" onClick={handleUpdate}>
+						Update
+					</button>
+				)}
 			</div>
 		</div>
 	);
