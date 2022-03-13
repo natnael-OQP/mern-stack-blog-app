6f0dff2f1bb75415385784f688329ac0c6e0d85a
papa-react <makiya112545@gmail.com>
2022-03-13 20:04:44 +0300

    handelsubmit completed

--- frontend/src/pages/settings/Settings.jsx
+++ frontend/src/pages/settings/Settings.jsx
@@ -1,8 +1,69 @@
 import "./settings.css";
 import Sidebar from "../../components/sidebar/Sidebar";
 import { FaUserCircle } from "react-icons/fa";
+import { useContext, useState } from "react";
+import { Context } from "../../context/Context";
+import axios from "axios";
+import Spinner from "../../components/spinner/Spinner";
 
 export default function Settings() {
+	const { user } = useContext(Context);
+	const [input, setInput] = useState();
+	const [file, setFile] = useState();
+	const [isLoading, setLoading] = useState(false);
+
+	const changeHandler = (e) => {
+		setInput((prev) => ({
+			...prev,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	const handelSubmit = async (e) => {
+		e.preventDefault();
+		const data = new FormData();
+		data.append("file", file);
+		data.append("upload_preset", "uploads");
+		try {
+			if (file) {
+				const upload = await axios.post(
+					`https://api.cloudinary.com/v1_1/pizza-oqp/image/upload`,
+					data
+				);
+				const { url } = upload.data;
+				const updated = {
+					...input,
+					userID: user._id,
+					profilePic: url,
+				};
+				const { data: newUser } = await axios.put(
+					"http://localhost:5000/api/user/" + user._id,
+					updated
+				);
+				localStorage.clear();
+				localStorage.setItem("user", JSON.stringify(newUser));
+				window.location.reload();
+			} else {
+				const post = {
+					...input,
+					userID: user._id,
+				};
+				const { data: newUser } = await axios.put(
+					"http://localhost:5000/api/user/" + user._id,
+					post
+				);
+				localStorage.clear();
+				localStorage.setItem("user", newUser);
+				window.location.reload();
+			}
+			setLoading(false);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	console.log(user);
+	if (isLoading) return <Spinner />;
+
 	return (
 		<div className="settings">
 			<div className="settingsWrapper">
@@ -12,13 +73,17 @@ export default function Settings() {
 					</span>
 					<span className="settingsTitleDelete">Delete Account</span>
 				</div>
-				<form className="settingsForm">
+				<form className="settingsForm" onSubmit={handelSubmit}>
 					<label>Profile Picture</label>
 					<div className="settingsPP">
-						<img
-							src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-							alt=""
-						/>
+						{user?.profilePic ? (
+							<img src={user?.profilePic} alt="" />
+						) : (
+							<img
+								src="https://imgs.search.brave.com/oQku-RrUxkdVYMEXTZfEssUEez3TAJ-FOyfWseMAUms/rs:fit:980:980:1/g:ce/aHR0cHM6Ly9jZG4u/b25saW5ld2ViZm9u/dHMuY29tL3N2Zy9p/bWdfMjk5NTg2LnBu/Zw"
+								alt=""
+							/>
+						)}
 						<label htmlFor="fileInput">
 							<div className="settingsPPIcon ">
 								<FaUserCircle />
@@ -27,23 +92,31 @@ export default function Settings() {
 						<input
 							id="fileInput"
 							type="file"
+							onChange={(e) => setFile(e.target.files[0])}
 							style={{ display: "none" }}
 							className="settingsPPInput"
 						/>
 					</div>
-					<label>Username</label>
-					<input type="text" placeholder="natnael-oqp" name="name" />
+					<label>username</label>
+					<input
+						type="text"
+						placeholder="natnael-oqp"
+						name="username"
+						onChange={changeHandler}
+					/>
 					<label>Email</label>
 					<input
 						type="email"
 						placeholder="natnael-oqp@gmail.com"
 						name="email"
+						onChange={changeHandler}
 					/>
 					<label>Password</label>
 					<input
 						type="password"
 						placeholder="Password"
 						name="password"
+						onChange={changeHandler}
 					/>
 					<button className="settingsSubmitButton" type="submit">
 						Update
